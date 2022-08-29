7cb35ad8787ad53effe35c8cabe27701b493f928
Vitaly Shatalov <vetka921@gmail.com>
2022-08-29 15:59:04 +0700

    feat(HTTP/2): Express http2 support test

--- src/base/index.ts
+++ src/base/index.ts
@@ -1,9 +1,21 @@
-import { createServer } from 'http'
+import http from 'http'
+import http2 from 'http2'
 import expressApp from './expressApp'
 import { patchServerWithIO } from './patchSocket'
+import { readFileSync } from 'fs'
+import { SERVER_CERT_PATH, SERVER_KEY_PATH } from '@/config/server'
 
-const httpServer = createServer(expressApp)
+//const httpServer = http.createServer(expressApp)
 
-const ioServer = patchServerWithIO(httpServer)
+const httpServer = http2.createSecureServer(
+  {
+    allowHTTP1: true,
+    key: readFileSync(SERVER_KEY_PATH),
+    cert: readFileSync(SERVER_CERT_PATH),
+  },
+  expressApp,
+)
+
+const ioServer = patchServerWithIO(httpServer as unknown as http.Server)
 
 export { ioServer, httpServer }
---
+++ src/config/server.ts
@@ -0,0 +1,2 @@
+export const SERVER_CERT_PATH = import.meta.env.VITE_SERVER_CERT_PATH
+export const SERVER_KEY_PATH = import.meta.env.VITE_SERVER_KEY_PATH
